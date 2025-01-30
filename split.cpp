84b9678b633beb4589ea4d7f8c91e2e0c1b60c68
Collin Hargreaves <chargrea@usc.edu>
2025-01-30 14:54:47 -0800

    completed part 1

--- split.cpp
+++ split.cpp
@@ -9,15 +9,45 @@ To test your program write a separate .cpp file and #include
 split.h.  **Do NOT add main() to this file**.  When you submit
 the function below should be the only one in this file.
 */
-
+#include <cstddef>
 #include "split.h"
 
 /* Add a prototype for a helper function here if you need */
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
-  /* Add code here */
-// WRITE YOUR CODE HERE
+
+  // Node*& is a reference to a pointer
+  // allows the function to modify the address the pointer is holding
+  // this allows us to point the pointer to a different node
+  
+  // base case
+  // if in is nullptr we have reached the end
+  // set odds and evens to null and return
+  if (in == nullptr) {
+    evens = nullptr;
+    odds = nullptr;
+    return;
+  }
+
+  // saving next node to call recursively
+  Node* saved_node = in->next;
+
+  // disconnect in node from list
+  in->next = nullptr;
+  
+  // check if even or odd
+  if (in->value % 2 == 0) {
+    // make evens point to the same pointer as in
+    evens = in;
+    in = nullptr;
+    split(saved_node, odds, evens->next);
+  } else {
+    odds = in;
+    in = nullptr;
+    split(saved_node, odds->next, evens);
+  }
+  
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -1,4 +1,5 @@
 /*
+ *
 CSCI 104: Homework 1 Problem 1
 
 Use this file to test your split.cpp file.
@@ -8,10 +9,59 @@ Create linked lists and split them with your split() function.
 You can compile this file like this:
 g++ split.cpp test_split.cpp -o test_split
 */
-
+#include <cstddef>
+#include <iostream>
 #include "split.h"
+
+using namespace std;
+
+void print_linkedlist(Node* head);
+void delete_linkedlist(Node* head);
 
 int main(int argc, char* argv[])
 {
 
+  Node* NodeOne = new Node(10, nullptr);
+  Node* NodeTwo = new Node(8, NodeOne);
+  Node* NodeThree = new Node(5, NodeTwo);
+  Node* NodeFour = new Node(2, NodeThree);
+
+  print_linkedlist(NodeFour);
+
+  Node* evens = nullptr;
+  Node* odds = nullptr;
+  split(NodeFour, odds, evens);
+  cout << "after split" << endl;
+  
+  cout << "evens" << endl;
+  print_linkedlist(evens);
+  cout << endl;
+  cout << "odds" << endl;
+  print_linkedlist(odds);
+  cout << endl;
+
+  delete_linkedlist(evens);
+  delete_linkedlist(odds);
 }
+
+void print_linkedlist(Node* head) {
+
+  if (head == nullptr) {
+    return;
+  }
+
+  cout << head->value << " "; 
+  print_linkedlist(head->next);
+  
+}
+
+void delete_linkedlist(Node* head) {
+
+  while (head != nullptr) {
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+  }
+
+}
+
