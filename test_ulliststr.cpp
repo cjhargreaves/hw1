a2642f2c5d9184a4ee73afa76fbf350d5061e7b3
Collin Hargreaves <chargrea@usc.edu>
2025-01-31 21:58:59 -0800

    completed part 2

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -6,8 +6,45 @@
 #include "ulliststr.h"
 
 //Use this file to test your ulliststr implementation before running the test suite
+//
+//
+using namespace std;
 
 int main(int argc, char* argv[])
 {
+  ULListStr *newList = new ULListStr();
+
+  
+  newList->push_front("hello");
+  newList->push_front("another");
+  newList->push_front("test");
+  newList->push_front("meow");
+  
+  // front will be meow
+  // back will be hello
+  cout << "front: " << newList->front() << endl;
+  cout << "back: " << newList->back() << endl;
+
+  // changing elements
+  newList->set(0, "first changed");
+  newList->set(1, "second changed");
+
+  cout << "index 1: " << newList->get(0) << endl;
+  cout << "index 2: " << newList->get(1) << endl;
+
+  newList->pop_front();
+  newList->pop_back();
+
+  // new front will be test
+  // new back will be another
+  cout << "new front: " << newList->front() << endl;
+  cout << "new back: " << newList->back() << endl;
+
+
+
+  newList->clear();
+
+  delete newList;
+
 
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -1,5 +1,6 @@
 #include <cstddef>
 #include <stdexcept>
+#include <string>
 #include "ulliststr.h"
 
 ULListStr::ULListStr()
@@ -25,6 +26,167 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+
+// add new string to node, if node is full or nullptr, need to create a new node
+void ULListStr::push_back(const std::string& val) {
+  // if no elements in list, or array in last node is full we cannot add more elements to it
+  // then we create a node 
+  if (tail_ == nullptr || tail_->last == ARRSIZE) {
+    Item* newNode = new Item;
+
+    // if the list is not empty
+    // we need to set the tail's next pointer to our new item
+    // and new item previous pointer to tail
+    if (tail_ != nullptr) {
+      tail_->next = newNode;
+      newNode->prev = tail_;
+    }
+
+    tail_ = newNode;
+    
+    // if the head is null, then this new node is the only one in the list
+    if (head_ == nullptr) {
+      head_ = tail_;
+    }
+  }
+  
+  // logic to add new string
+  std::string *arr = tail_->val;
+  int currentIdx = tail_->last;
+  arr[currentIdx] = val;
+  tail_->last = currentIdx + 1;
+  
+  // increment size
+  size_++;
+}
+
+// similar to push_back
+void ULListStr::push_front(const std::string& val) {
+  // check for elements in list
+  if (head_ == nullptr || head_->first == 0) {
+    Item* newNode = new Item;
+    newNode->last = ARRSIZE; 
+    newNode->first = ARRSIZE;
+    
+    // if the list is not empty
+    // need to set the new node's next pointer to the head
+    // and head prev to the new node
+    if (head_ != nullptr) {
+      newNode->next =head_;
+      head_->prev = newNode;
+
+    } else {
+      // list was empty? this new node is also tail
+      tail_ = newNode;
+
+    }
+    head_ = newNode;
+
+  }
+
+  // logic to add new string
+  std::string *arr = head_->val;
+  int currentIdx = head_->first - 1;
+  arr[currentIdx] = val;
+  head_->first = currentIdx;
+
+  size_++;
+}
+
+// general idea for pop and push
+// check if empty
+// pop_back - decrease the last index by 1 (moves the pointer)
+// pop_front - increase the index by 1 
+// check now if the array is empty
+// remove node if empty
+// adjust size
+
+void ULListStr::pop_back() {
+  // check if llist is empty
+  if (empty()) {
+    return;
+  }
+
+  // check the decrement of last with first
+  // if its the same, then its empty
+  if (--tail_->last == tail_->first) {
+
+    Item* temp = tail_;
+    tail_ = tail_->prev;
+
+    if (tail_) {
+      tail_->next = nullptr;
+    } else  {
+      head_ = nullptr;
+    }
+    delete temp;
+  }
+
+
+  size_--;
+}
+
+void ULListStr::pop_front() {
+  // check empty
+  if (empty()) {
+    return;
+  }
+
+  // check if array is empty
+  if (++head_->first == head_->last) {
+    Item* temp = head_;
+    head_ = head_->next;
+
+    if (head_) {
+      head_->prev = nullptr;
+    } else {
+      tail_ = nullptr;
+    }
+
+
+    delete temp;
+  }
+
+  size_--;
+}
+
+std::string const & ULListStr::back() const {
+
+  return tail_->val[tail_->last - 1];
+
+}
+
+std::string const & ULListStr::front() const {
+
+  return head_->val[head_->first];
+
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const {
+  // if loc >= size then this loc does not exist
+  if (loc >= size_) {
+    return nullptr;
+  } 
+  
+  // traverse list
+  Item* current = head_;
+  while (current != nullptr) {
+
+    int index = current->first + loc;
+    
+    if (index >= current->first && index < current->last) {
+      return &current->val[index];
+    }
+
+    int elements = current->last - current->first;
+    loc = loc - elements;
+    current = current->next;
+
+  }
+  return nullptr;
+}
+
+
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
